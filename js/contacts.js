16988ea61d079103c2dd2b167640761c23934ca0
Mariya <masha.shynkaruk@gmail.com>
2019-02-28 14:50:59 +0200

    change in files

--- js/contacts.js
+++ js/contacts.js
@@ -1,4 +1,4 @@
-(function() {
+/*(function() {
     'use strict';
     window.addEventListener('load', function() {
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
@@ -14,4 +14,56 @@
             }, false);
         });
     }, false);
-})();
+})();*/
+
+$(function() {
+
+    /*  validate form  */
+    $("form").each(function() {
+        $(this).validate({
+            rules: {
+                FirstName: {
+                    required: true,
+                    minlength: 3
+                },
+                LastName: {
+                    required: true,
+                    minlength: 3
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                Phone: {
+                    required: true
+                },
+                password: {
+                    required: true,
+                    minlength: 5
+                }
+            },
+            messages: {
+                FirstName: {
+                    required: "Имя не может быть пустым",
+                    minlength: jQuery.validator.format("Введите не менее {0} символов.")
+                },
+                LastName: {
+                    required: "Фамилия не может быть пустым",
+                    minlength: jQuery.validator.format("Введите не менее {0} символов.")
+                },
+                email: {
+                    required: "Email не может быть пустым",
+                    email: "Неверный формат Email"
+                },
+                Phone: {
+                    required: "Телефон не может быть пустым"
+                },
+                password: {
+                    required: "Пароль не может быть пустым",
+                    minlength: jQuery.validator.format("Введите не менее {0} символов")
+                }
+            }
+        });
+    });
+    /*  validate form  */
+});
